3c964fd08d64b0ac475baa5c4ef1cc237fb2a54d
dmanh289 <manh.teras@gmail.com>
2025-06-23 17:23:35 +0700

    fix find target

--- Assets/Scripts/JobSystem/CaseStudy4/FindClosestTargetJob.cs
+++ Assets/Scripts/JobSystem/CaseStudy4/FindClosestTargetJob.cs
@@ -10,33 +10,35 @@ namespace JobSystem.CaseStudy4
     {
         [ReadOnly] public NativeArray<float3> UnitPositions;
         [ReadOnly] public NativeArray<float3> TargetPositions;
-        [ReadOnly] public NativeArray<float> UnitRanges;
-        [ReadOnly] public NativeArray<bool> TargetStates;
+        // [ReadOnly] public NativeArray<float> UnitRanges;
+        // [ReadOnly] public NativeArray<bool> TargetStates;
         
         [WriteOnly] public NativeArray<int> TargetIndices;
         
         
         public void Execute(int index)
         {
-            if (UnitRanges[index] <= 0)
-            {
-                TargetIndices[index] = -1;
-                return;
-            }
+            // if (UnitRanges[index] <= 0)
+            // {
+            //     TargetIndices[index] = -1;
+            //     return;
+            // }
+            
+            TargetIndices[index] = -1;
 
             float3 unitPos = UnitPositions[index];
-            float attackRange = UnitRanges[index];
+            // float attackRange = UnitRanges[index];
 
             int closestTargetIndex = -1;
             float closestDistance = float.MaxValue;
 
             for (int i = 0; i < TargetPositions.Length; i++)
             {
-                if (!TargetStates[i]) continue;
+                // if (!TargetStates[i]) continue;
                 
                 float distance = math.distance(unitPos, TargetPositions[i]);
 
-                if (distance <= attackRange && distance < closestDistance)
+                if (distance <= 10f && distance < closestDistance)
                 {
                     closestDistance = distance;
                     closestTargetIndex = i;
--- Assets/Scripts/JobSystem/CaseStudy4/TargetSelector.cs
+++ Assets/Scripts/JobSystem/CaseStudy4/TargetSelector.cs
@@ -17,19 +17,19 @@ namespace JobSystem.CaseStudy4
         
         // Ally data
         private NativeArray<float3> allyPositions;
-        private NativeArray<float> allyAttackRanges;
+        // private NativeArray<float> allyAttackRanges;
         private NativeArray<int> targetOfAllies;
         private NativeArray<bool> allyActiveStates;
         
         // Enemy data
         private NativeArray<float3> enemyPositions;
-        private NativeArray<float> enemyAttackRanges;
+        // private NativeArray<float> enemyAttackRanges;
         private NativeArray<int> targetOfEnemies;
-        private NativeArray<bool> enemyAttackableStates;
-        private NativeArray<Quaternion> enemyRotations;
-        private NativeArray<bool> useRectCheck;
-        private NativeArray<float2> enemyRectSize;
-        private NativeArray<float> enemyRadiusCheck;
+        // private NativeArray<bool> enemyAttackableStates;
+        // private NativeArray<Quaternion> enemyRotations;
+        // private NativeArray<bool> useRectCheck;
+        // private NativeArray<float2> enemyRectSize;
+        // private NativeArray<float> enemyRadiusCheck;
         private NativeArray<bool> hasAllyInRange;
         
         // Player data
@@ -53,18 +53,18 @@ namespace JobSystem.CaseStudy4
             int initialSize = 10;
         
             allyPositions = new NativeArray<float3>(initialSize, Allocator.Persistent);
-            allyAttackRanges = new NativeArray<float>(initialSize, Allocator.Persistent);
+            //allyAttackRanges = new NativeArray<float>(initialSize, Allocator.Persistent);
             targetOfAllies = new NativeArray<int>(initialSize, Allocator.Persistent);
             allyActiveStates = new NativeArray<bool>(initialSize, Allocator.Persistent);
 
             enemyPositions = new NativeArray<float3>(initialSize, Allocator.Persistent);
-            enemyAttackRanges = new NativeArray<float>(initialSize, Allocator.Persistent);
+            //enemyAttackRanges = new NativeArray<float>(initialSize, Allocator.Persistent);
             targetOfEnemies = new NativeArray<int>(initialSize, Allocator.Persistent);
-            enemyAttackableStates = new NativeArray<bool>(initialSize, Allocator.Persistent);
-            enemyRotations = new NativeArray<Quaternion>(initialSize, Allocator.Persistent);
-            useRectCheck = new NativeArray<bool>(initialSize, Allocator.Persistent);
-            enemyRectSize = new NativeArray<float2>(initialSize, Allocator.Persistent);
-            enemyRadiusCheck = new NativeArray<float>(initialSize, Allocator.Persistent);
+            // enemyAttackableStates = new NativeArray<bool>(initialSize, Allocator.Persistent);
+            // enemyRotations = new NativeArray<Quaternion>(initialSize, Allocator.Persistent);
+            // useRectCheck = new NativeArray<bool>(initialSize, Allocator.Persistent);
+            // enemyRectSize = new NativeArray<float2>(initialSize, Allocator.Persistent);
+            // enemyRadiusCheck = new NativeArray<float>(initialSize, Allocator.Persistent);
             hasAllyInRange = new NativeArray<bool>(initialSize, Allocator.Persistent);
 
             activePlayerAttack = new NativeReference<bool>(Allocator.Persistent);
@@ -87,7 +87,7 @@ namespace JobSystem.CaseStudy4
             if (allies.Count != previousAllyCount)
             {
                 ReinitializeNativeArray(ref allyPositions, allyCount);
-                ReinitializeNativeArray(ref allyAttackRanges, allyCount);
+                // ReinitializeNativeArray(ref allyAttackRanges, allyCount);
                 ReinitializeNativeArray(ref targetOfAllies, allyCount);
                 ReinitializeNativeArray(ref allyActiveStates, allyCount);
                 previousAllyCount = allyCount;
@@ -96,13 +96,13 @@ namespace JobSystem.CaseStudy4
             if(enemyCount != previousEnemyCount)
             {
                 ReinitializeNativeArray(ref enemyPositions, enemyCount);
-                ReinitializeNativeArray(ref enemyAttackRanges, enemyCount);
+                // ReinitializeNativeArray(ref enemyAttackRanges, enemyCount);
                 ReinitializeNativeArray(ref targetOfEnemies, enemyCount);
-                ReinitializeNativeArray(ref enemyAttackableStates, enemyCount);
-                ReinitializeNativeArray(ref enemyRotations, enemyCount);
-                ReinitializeNativeArray(ref useRectCheck, enemyCount);
-                ReinitializeNativeArray(ref enemyRectSize, enemyCount);
-                ReinitializeNativeArray(ref enemyRadiusCheck, enemyCount);
+                // ReinitializeNativeArray(ref enemyAttackableStates, enemyCount);
+                // ReinitializeNativeArray(ref enemyRotations, enemyCount);
+                // ReinitializeNativeArray(ref useRectCheck, enemyCount);
+                // ReinitializeNativeArray(ref enemyRectSize, enemyCount);
+                // ReinitializeNativeArray(ref enemyRadiusCheck, enemyCount);
                 ReinitializeNativeArray(ref hasAllyInRange, enemyCount);
                 previousEnemyCount = enemyCount;
             }
@@ -114,19 +114,48 @@ namespace JobSystem.CaseStudy4
             {
                 var ally = allies[i];
                 allyPositions[i] = ally.transform.position;
-                allyAttackRanges[i] = 100;
+                //allyAttackRanges[i] = 100;
                 allyActiveStates[i] = ally.gameObject.activeSelf;
             }
             
+            for(int i = 0 ; i < enemies.Count; ++i)
+            {
+                var enemy = enemies[i];
+                enemyPositions[i] = enemy.transform.position;
+                // enemyAttackRanges[i] = 100;
+                // enemyAttackableStates[i] = true; // Assume all enemies are attackable
+                // enemyRotations[i] = enemy.transform.rotation;
+                // useRectCheck[i] = false; // Default to false, can be set based on specific logic
+                // enemyRectSize[i] = new float2(5f, 5f); // Example size, can be adjusted
+                // enemyRadiusCheck[i] = 10f; // Example radius, can be adjusted
+            }
+        }
+
+        private void ScheduleTargetingJob()
+        {
+            findAllyTargetJob = new FindClosestTargetJob()
+            {
+                UnitPositions = allyPositions,
+                TargetPositions = enemyPositions,
+                TargetIndices = targetOfAllies
+            };
+            allyTargetJobHandle = findAllyTargetJob.Schedule(allies.Count, 64);
             
+            findEnemyTargetJob = new FindClosestTargetJob()
+            {
+                UnitPositions = enemyPositions,
+                TargetPositions = allyPositions,
+                TargetIndices = targetOfEnemies
+            };
+            enemyTargetJobHandle = findEnemyTargetJob.Schedule(allies.Count, 64);
         }
 
         private void CompleteAllJobs()
         {
             allyTargetJobHandle.Complete();
             enemyTargetJobHandle.Complete();
-            playerAttackJobHandle.Complete();
-            enemyAttackJobHandle.Complete();
+            // playerAttackJobHandle.Complete();
+            // enemyAttackJobHandle.Complete();
         }
         
         private void ReinitializeNativeArray<T>(ref NativeArray<T> array, int newSize) where T : struct
@@ -136,6 +165,52 @@ namespace JobSystem.CaseStudy4
                 array.Dispose();
             }
             array = new NativeArray<T>(newSize, Allocator.Persistent);
+        }
+
+        private void LateUpdate()
+        {
+            if (!isStartSelectTarget) return;
+            
+            CompleteAllJobs();
+        }
+
+        private void ApplyJobResult()
+        {
+            for(int i = 0; i < allies.Count; ++i)
+            {
+                var ally = allies[i];
+                int targetIndex = targetOfAllies[i];
+                
+                if (targetIndex >= 0 && targetIndex < enemies.Count)
+                {
+                    var targetEnemy = enemies[targetIndex];
+                    // Do something with the target enemy, e.g., set a target on the ally
+                    // ally.SetTarget(targetEnemy);
+                }
+                else
+                {
+                    // No valid target found, handle accordingly
+                    // ally.ClearTarget();
+                }
+            }
+            
+            for(int i = 0; i < enemies.Count; ++i)
+            {
+                var enemy = enemies[i];
+                int targetIndex = targetOfEnemies[i];
+                
+                if (targetIndex >= 0 && targetIndex < allies.Count)
+                {
+                    var targetAlly = allies[targetIndex];
+                    // Do something with the target ally, e.g., set a target on the enemy
+                    // enemy.SetTarget(targetAlly);
+                }
+                else
+                {
+                    // No valid target found, handle accordingly
+                    // enemy.ClearTarget();
+                }
+            }
         }
     }
 }
